aeeef6a64314ec8fbf993b6b56b9f0e173934d42
unknown <marco.fernandes.inf10@hotmail.com>
2020-11-17 18:36:24 +0000

    Updates

---
+++ precache-manifest.a7bdbb3451a757c437babd81f42029e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ac420c6c9ca2fd23c41f62c2fddb16",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0e0ab70cd27c484ecabd",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f26be2312cff19045657",
+    "url": "/portfolio/static/css/main.93e4647b.chunk.css"
+  },
+  {
+    "revision": "0e0ab70cd27c484ecabd",
+    "url": "/portfolio/static/js/2.3dfaf7ae.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/portfolio/static/js/2.3dfaf7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26be2312cff19045657",
+    "url": "/portfolio/static/js/main.3c5ca507.chunk.js"
+  },
+  {
+    "revision": "1641c9edd375c93fcb66",
+    "url": "/portfolio/static/js/runtime-main.bf81aa83.js"
+  },
+  {
+    "revision": "92d20dd00ee7cdc7dc9b778757dfb542",
+    "url": "/portfolio/static/media/me.92d20dd0.png"
+  }
+]);
